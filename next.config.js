775e04e0d83cfa1ea77fcc02965f9e8e89a360b6
rayniel95 <rainyel.ramos@gmail.com>
2023-07-16 17:34:44 -0400

    try to add a basse path

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
+    basePath: '/blockexplorer',
+    assetPrefix:'/blockexplorer/',
     experimental: {
         appDir: true,
     },
