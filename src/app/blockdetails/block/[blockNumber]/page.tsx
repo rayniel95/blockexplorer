614fc2eaccb2093f8cffb06c9b0ce7bf818c8ac2
rayniel95 <rainyel.ramos@gmail.com>
2023-07-25 06:59:58 -0400

    add base code for details block view

--- src/app/blockdetails/block/[blockNumber]/page.tsx
+++ src/app/blockdetails/block/[blockNumber]/page.tsx
@@ -1,23 +1,37 @@
 'use client'
 
 import React, { Dispatch, SetStateAction, useState, useEffect } from "react";
-import List from "./components/Lists";
 import { EthereumManager } from "@/src/stateManager/blockchainManager/ethereum/ethereumManager";
-import { BlockItem } from "@/src/components/ethereum/blockItem";
-import { useRouter } from "next/router";
+import { BlockWithTransactionData } from "ethereum-types";
 
 
 const manager = new EthereumManager();
 
 
-export default function BlockDetails() {
-	const router = useRouter();
-	const { blockNumber } = router.query;
+export default function BlockDetails({params}: {params:{blockNumber:string}}) {
+	const [block, setBlock] = useState<BlockWithTransactionData>({} as BlockWithTransactionData);
+	const blockNumber = parseInt(params.blockNumber);
 
+	useEffect(() => {
+		const fetchData = async () => {
+			try {
+				//TODO - add loading item to items that read from network
+				const response = (await manager.getBlocks(blockNumber, blockNumber+1))[0]
+				setBlock(response);
+			} catch (error) {
+				console.error('Error:', error);
+			}
+		};
+
+		// Fetch data initially
+		fetchData();
+	});
 
 	return (
 		<div>
 			here are the block {blockNumber} details
+
+			<p>{JSON.stringify(block)}</p>
 		</div>
 	);
 }
