dcd6b0bde8ffae38767cfb4c9f6189ac812293cf
rayniel95 <rainyel.ramos@gmail.com>
2023-07-26 13:56:55 -0400

    add comment

--- src/src/stateManager/blockchainManager/ethereum/index.ts
+++ src/src/stateManager/blockchainManager/ethereum/index.ts
@@ -1,5 +1,6 @@
 import { Alchemy, Network } from "alchemy-sdk";
-
+//TODO - use the network drop down selector for switch between mainnet and 
+// testnet
 const settings = {
 	apiKey: process.env.NEXT_PUBLIC_ETHEREUM_API_KEY,
 	network: Network.ETH_MAINNET,
