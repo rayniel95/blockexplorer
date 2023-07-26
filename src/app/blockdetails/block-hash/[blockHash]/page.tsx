375bbe889a1b05e222897891337fcdd1db0c071f
rayniel95 <rainyel.ramos@gmail.com>
2023-07-26 07:25:30 -0400

    create new path for block details

---
+++ src/app/blockdetails/block-hash/[blockHash]/page.tsx
@@ -0,0 +1,37 @@
+'use client'
+
+import React, { Dispatch, SetStateAction, useState, useEffect } from "react";
+import { EthereumManager } from "@/src/stateManager/blockchainManager/ethereum/ethereumManager";
+import { BlockWithTransactionData } from "ethereum-types";
+
+
+const manager = new EthereumManager();
+
+
+export default function BlockDetails({params}: {params:{blockNumber:string}}) {
+	const [block, setBlock] = useState<BlockWithTransactionData>({} as BlockWithTransactionData);
+	const blockNumber = parseInt(params.blockNumber);
+
+	useEffect(() => {
+		const fetchData = async () => {
+			try {
+				//TODO - add loading item to items that read from network
+				const response = (await manager.getBlocks(blockNumber, blockNumber+1))[0]
+				setBlock(response);
+			} catch (error) {
+				console.error('Error:', error);
+			}
+		};
+
+		// Fetch data initially
+		fetchData();
+	});
+
+	return (
+		<div>
+			here are the block {blockNumber} details
+
+			<p>{JSON.stringify(block)}</p>
+		</div>
+	);
+}
