0c836b740859617ca46291c7b312d9d04fc3ea32
rayniel95 <rainyel.ramos@gmail.com>
2023-07-26 17:07:29 -0400

    add base component with icons and info about tx

--- src/app/components/commons/TransactionHash.tsx
+++ src/app/components/commons/TransactionHash.tsx
@@ -1,26 +1,28 @@
-import { Col, Row } from "react-bootstrap";
-import Container from "react-bootstrap/Container";
+'use client'
 
-export default function Footer() {
-//TODO - add color themes. research about it
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-icons/font/bootstrap-icons.css'
+
+import { BlockWithTransactions, TransactionReceipt, TransactionResponse } from "alchemy-sdk";
+import { Col, OverlayTrigger, Row, Tooltip } from "react-bootstrap";
+
+
+export default function TransactionHash({ tx }: { tx: TransactionResponse }) {
+  //TODO - add color themes. research about it
   return (
-    <footer>
-      <Container>
-        <Row>
-          <Col>
-            <p>
-              Made with ❤️ by <a href="https://rayniel95.github.io/">Rainyel Ramos</a>
-            </p>
-          </Col>
-          <Col>
-            <p>
-              <a href="https://github.com/rayniel95/ecdsa-node">
-                <i className="bi bi-github"></i> take a look to the code
-              </a>
-            </p>
-          </Col>
-        </Row>
-      </Container>
-    </footer>
+    <div>
+      <OverlayTrigger
+        placement='top'
+        overlay={
+          <Tooltip id="tooltip-top-hash">
+            Transaction hash: {tx.hash}
+          </Tooltip>
+        }
+      >
+        <p>
+          <i className="bi bi-hash"></i> {`${tx.hash.toString().slice(0, 5)}...`}
+        </p>
+      </OverlayTrigger>
+    </div>
   );
 }
