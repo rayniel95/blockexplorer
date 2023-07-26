3158377ea42c1df2cae30cb4d28cedc977f7b5fa
rayniel95 <rainyel.ramos@gmail.com>
2023-07-26 07:28:03 -0400

    add basic routes

--- src/src/settings.ts
+++ src/src/settings.ts
@@ -1,1 +1,7 @@
 //TODO - here write the routes to have a centric place for them
+export const BLOCKLIST_ROUTE = "/blocklist";
+export const BLOCKDETAILSHASH_ROUTE = "/blockdetails/block-hash";
+export const BLOCKDETAILSNUMBER_ROUTE = "/blockdetails/block-number";
+export const HUFFVERIFIER_ROUTE = "/huffverifier";
+export const TRANSACTIONDETAILS_ROUTE = "/transactiondetails/transaction-hash";
+export const BASE_PATH = "/blockexplorer";
