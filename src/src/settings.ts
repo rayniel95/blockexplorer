4a045b9eaa376defadb49a7e94927fa76fa4cc46
rayniel95 <rainyel.ramos@gmail.com>
2023-07-27 18:19:57 -0400

    change transaction list path to a more readable one

--- src/src/settings.ts
+++ src/src/settings.ts
@@ -4,5 +4,5 @@ export const BLOCKDETAILSHASH_ROUTE = "/blockdetails/block-hash";
 export const BLOCKDETAILSNUMBER_ROUTE = "/blockdetails/block-number";
 export const HUFFVERIFIER_ROUTE = "/huffverifier";
 export const TRANSACTIONDETAILS_ROUTE = "/transactiondetails/transaction-hash";
-export const TRANSACTIONLIST_ROUTE = "/transactionlist";
+export const TRANSACTIONLIST_ROUTE = "/transactionlist/block";
 export const BASE_PATH = "/blockexplorer";
