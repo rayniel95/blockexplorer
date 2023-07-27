b8c46b173cc1440ed46f798bb605d6bd30285301
rayniel95 <rainyel.ramos@gmail.com>
2023-07-27 11:14:13 -0400

    add block details component

--- src/app/blockdetails/components/BlockDetails.tsx
+++ src/app/blockdetails/components/BlockDetails.tsx
@@ -4,14 +4,30 @@ import React, { Dispatch, SetStateAction, useState, useEffect } from "react";
 import { EthereumManager } from "@/src/stateManager/blockchainManager/ethereum/ethereumManager";
 import { BlockWithTransactionData } from "ethereum-types";
 import { BlockWithTransactions } from "alchemy-sdk";
-
+import BlockNumber from "@/app/components/commons/block/BlockNumber";
+import BlockTimestamp from "@/app/components/commons/block/BlockTimestamp";
+import BlockNumberOfTx from "@/app/components/commons/block/BlockNumberOfTx";
+import BlockFeeRecipient from "@/app/components/commons/block/BlockFeeRecipient";
+import Link from "next/link";
+import * as settings from "@/src/settings"
+import BlockDifficulty from "@/app/components/commons/block/BlockDifficulty";
+import BlockGasUsed from "@/app/components/commons/block/BlockGasUsed";
+import BlockExtraData from "@/app/components/commons/block/BlockExtraData";
+import BlockHash from "@/app/components/commons/block/BlockHash";
+import BlockSize from "@/app/components/commons/block/BlockSize";
+//FIXME - use the link component from next instead a component. it add
+// base path automatically
 
 export default function BlockDetails({block}: {block: BlockWithTransactions}) {
 	return (
 		<div>
-			here are the block {block.number} details
-
-			<p>{JSON.stringify(block)}</p>
+			<BlockNumber block={block} />
+			<BlockHash block={block} />
+			<BlockTimestamp block={block} />
+			<Link href={settings.TRANSACTIONLIST_ROUTE}><BlockNumberOfTx block={block} /></Link>
+			<BlockFeeRecipient block={block} />
+			<BlockGasUsed block={block} />
+			<BlockExtraData block={block} />
 		</div>
 	);
 }
