d9a5c08897a793e5250f84d6feb0f38e894f6957
rayniel95 <rainyel.ramos@gmail.com>
2023-07-28 09:44:41 -0400

    add transaction details components

---
+++ src/app/components/commons/transaction/TransactionGasLimit.tsx
@@ -0,0 +1,30 @@
+'use client'
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-icons/font/bootstrap-icons.css'
+
+import { BlockWithTransactions, TransactionResponse } from "alchemy-sdk";
+import { Col, OverlayTrigger, Row, Tooltip } from "react-bootstrap";
+import { useState, useEffect } from 'react';
+import { useTimeDiff } from '@/src/stateManager/hooks';
+
+
+export default function TransactionGasLimit({ tx }: { tx: TransactionResponse }) {
+  //TODO - add color themes. research about it
+  return (
+    <div>
+      <OverlayTrigger
+        placement='top'
+        overlay={
+          <Tooltip id="tooltip-top-hash">
+            Transaction gas limit: {tx.gasLimit.toString()}
+          </Tooltip>
+        }
+      >
+        <p>
+          <i className="bi bi-fuel-pump-fill"></i> {tx.gasLimit.toString().slice(0, 5)}...
+        </p>
+      </OverlayTrigger>
+    </div>
+  );
+}
---
+++ src/app/components/commons/transaction/TransactionValue.tsx
@@ -0,0 +1,29 @@
+'use client'
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-icons/font/bootstrap-icons.css'
+
+import { BigNumber, BlockWithTransactions, TransactionReceipt, TransactionResponse } from "alchemy-sdk";
+import { Col, OverlayTrigger, Row, Tooltip } from "react-bootstrap";
+import {Utils} from 'alchemy-sdk';
+
+
+export default function TransactionValue({ tx }: { tx: TransactionResponse }) {
+  //TODO - add color themes. research about it
+  return (
+    <div>
+      <OverlayTrigger
+        placement='top'
+        overlay={
+          <Tooltip id="tooltip-top-hash">
+            Transaction value: {Utils.formatEther(tx.value.toString()).toString()} ETH
+          </Tooltip>
+        }
+      >
+        <p>
+          <i className="bi bi-coin"></i> {`${tx.value.toString().slice(0, 5)}...`}
+        </p>
+      </OverlayTrigger>
+    </div>
+  );
+}
