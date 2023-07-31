fffbf72d61c3d85c458013cda6afdfc1ccb6b653
rayniel95 <rainyel.ramos@gmail.com>
2023-07-31 08:32:39 -0400

    add address details components

---
+++ src/app/addressdetails/[address]/components/AddressBalance.tsx
@@ -0,0 +1,28 @@
+'use client'
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-icons/font/bootstrap-icons.css'
+
+import { BlockWithTransactions } from "alchemy-sdk";
+import { Col, OverlayTrigger, Row, Tooltip } from "react-bootstrap";
+
+
+export default function AddressBalance({ balance }: { balance: string }) {
+  //TODO - add color themes. research about it
+  return (
+    <div>
+      <OverlayTrigger
+        placement='top'
+        overlay={
+          <Tooltip id="tooltip-top-hash">
+            Address balance: {balance} ETH
+          </Tooltip>
+        }
+      >
+        <p>
+          <i className="bi bi-piggy-bank-fill"></i> {`${balance.toString().slice(0, 5)}...`}
+        </p>
+      </OverlayTrigger>
+    </div>
+  );
+}
---
+++ src/app/addressdetails/[address]/components/AddressTransactionCount.tsx
@@ -0,0 +1,28 @@
+'use client'
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-icons/font/bootstrap-icons.css'
+
+import { BlockWithTransactions } from "alchemy-sdk";
+import { Col, OverlayTrigger, Row, Tooltip } from "react-bootstrap";
+
+
+export default function AddressTransactionCount({ transactionCount }: { transactionCount: number }) {
+  //TODO - add color themes. research about it
+  return (
+    <div>
+      <OverlayTrigger
+        placement='top'
+        overlay={
+          <Tooltip id="tooltip-top-hash">
+            Address transaction count: {transactionCount}
+          </Tooltip>
+        }
+      >
+        <p>
+          <i className="bi bi-wallet2"></i> {`${transactionCount.toString().slice(0, 5)}...`}
+        </p>
+      </OverlayTrigger>
+    </div>
+  );
+}
