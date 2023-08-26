16328ac4702df9cc38aa21350e4b87f7c8532ee5
rayniel95 <rainyel.ramos@gmail.com>
2023-08-26 15:22:08 -0400

    test huff wasm

--- next.config.js
+++ next.config.js
@@ -6,6 +6,11 @@ const nextConfig = {
     experimental: {
         appDir: true,
     },
+    webpack(config) {
+        // config.output.webassemblyModuleFilename = 'static/wasm/[modulehash].wasm'
+        config.experiments = { asyncWebAssembly: true }
+        return config
+    },
 }
 
 module.exports = nextConfig
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,42 +2,65 @@
 
 import { useState, useEffect } from "react";
 import { Col, Container, ListGroup, Row } from "react-bootstrap";
-import { EthereumManager } from "@/src/stateManager/blockchainManager/ethereum/ethereumManager";
 import { BlockItem } from "@/app/components/commons/BlockItem";
 import { TransactionItem } from "@/app/components/commons/TransactionItem";
 import { useAppSelector } from "@/src/stateManager/hooks";
 import { ethereumManager } from "@/src/stateManager/blockchainManager/ethereum";
+import { compile } from "../huff-bundler"
+
 
 //TODO - add horizontal scroll to text in address, etc.
 export default function Home() {
 	const [blocks, setBlocks] = useState([]);
 	const network = useAppSelector((state) => state.network.newtork);
-	
-	useEffect(() => {
-		ethereumManager.config(network);
-		const fetchData = async () => {
-			try {
-				const response = await ethereumManager.getBlocksFromNegativeIndex(0, 5)
-				setBlocks(response);
-			} catch (error) {
-				console.error('Error:', error);
-			}
-		};
 
-		// Fetch data initially
-		fetchData()
-		// Fetch data every five seconds
-		const intervalId = setInterval(fetchData, 5000);
 
-		// Clean up interval timer when component unmounts
-		return () => {
-			clearInterval(intervalId);
-		};
-	}, [network]);
+	let files = {
+		"add.huff": "#define function add(uint256,uint256) nonpayable returns (uint256)\n" +
+			"\n" +
+			"#define macro MAIN() = {\n" +
+			"   // Load our numbers from calldata and add them together.\n" +
+			"   0x04 calldataload // [number1]\n" +
+			"   0x24 calldataload // [number2]\n" +
+			"   add               // [number1+number2]\n" +
+			"   // Return our new number.\n" +
+			"   0x00 mstore // Store our number in memory.\n" +
+			"   0x20 0x00 return // Return it.\n" +
+			"}\n"
+	}
+
+	const result = compile({
+		files,
+		sources: ['add.huff']
+	});
+
+	console.log(result)
+
+	// useEffect(() => {
+	// 	ethereumManager.config(network);
+	// 	const fetchData = async () => {
+	// 		try {
+	// 			const response = await ethereumManager.getBlocksFromNegativeIndex(0, 5)
+	// 			setBlocks(response);
+	// 		} catch (error) {
+	// 			console.error('Error:', error);
+	// 		}
+	// 	};
+
+	// 	// Fetch data initially
+	// 	fetchData()
+	// 	// Fetch data every five seconds
+	// 	const intervalId = setInterval(fetchData, 5000);
+
+	// 	// Clean up interval timer when component unmounts
+	// 	return () => {
+	// 		clearInterval(intervalId);
+	// 	};
+	// }, [network]);
 
 	return (
 		<Container fluid>
-			<Row>
+			{/* <Row>
 				<Col>
 					<ListGroup>
 						{blocks.reverse().map((response, index) => {
@@ -62,7 +85,7 @@ export default function Home() {
 						}
 					</ListGroup>
 				</Col>
-			</Row>
+			</Row> */}
 		</Container>
 	);
 }
