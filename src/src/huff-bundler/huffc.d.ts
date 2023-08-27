88050d5dd861880c88466dd5d98843d623fa8a87
rayniel95 <rainyel.ramos@gmail.com>
2023-08-27 15:37:45 -0400

    add wasm for bundler webpack

---
+++ src/src/huff-bundler/huffc.d.ts
@@ -0,0 +1,8 @@
+/* tslint:disable */
+/* eslint-disable */
+/**
+* Compiles contracts based on supplied JSON input
+* @param {any} input
+* @returns {any}
+*/
+export function compile(input: any): any;
---
+++ src/src/huff-bundler/huffc.js
@@ -0,0 +1,4 @@
+import * as wasm from "./huffc_bg.wasm";
+import { __wbg_set_wasm } from "./huffc_bg.js";
+__wbg_set_wasm(wasm);
+export * from "./huffc_bg.js";
