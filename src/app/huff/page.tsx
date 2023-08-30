9a868dce198e7c23a6a99aff16b46a862a2af4d2
rayniel95 <rainyel.ramos@gmail.com>
2023-08-30 08:06:34 -0400

    show bytecode

--- src/app/huff/page.tsx
+++ src/app/huff/page.tsx
@@ -1,35 +1,20 @@
 'use client'
 
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 import { Button, Container, Form } from 'react-bootstrap'
 
-let files = {
-	"add.huff": "#define function add(uint256,uint256) nonpayable returns (uint256)\n" +
-		"\n" +
-		"#define macro MAIN() = {\n" +
-		"   // Load our numbers from calldata and add them together.\n" +
-		"   0x04 calldataload // [number1]\n" +
-		"   0x24 calldataload // [number2]\n" +
-		"   add               // [number1+number2]\n" +
-		"   // Return our new number.\n" +
-		"   0x00 mstore // Store our number in memory.\n" +
-		"   0x20 0x00 return // Return it.\n" +
-		"}\n"
-}
-
 /*
-  const huffFileName = 'main.huff'
-		const compiledHuff = compile({
-		  files: {
-			[huffFileName]: code,
-		  },
-		  sources: [huffFileName],
-		})
-		const { bytecode } = compiledHuff.contracts.get(huffFileName)
-		loadInstructions(bytecode)
-		startExecution(bytecode, _callValue, _callData)
+#define function add(uint256,uint256) nonpayable returns (uint256)
+	#define macro MAIN() = {
+	// Load our numbers from calldata and add them together.
+	   0x04 calldataload // [number1]
+	   0x24 calldataload // [number2]
+	   add               // [number1+number2]
+	   // Return our new number.\n" +
+	   0x00 mstore // Store our number in memory.
+		0x20 0x00 return // Return it.
+	}
 */
-
 let compile: any = undefined
 async function loadCompile() {
 	const huffc = await import("../../src/huff-bundler/huffc")
@@ -44,16 +29,23 @@ export default function HuffVerifier() {
 	const [code, setCode] = useState('')
 	const [address, setAddress] = useState('')
 	const [bytecode, setBytecode] = useState('')
+	const [error, setError] = useState('')
 
 	function verify(e: React.FormEvent<HTMLFormElement>) {
 		e.preventDefault()
-		const compiledHuff = compile({
-			files: {
-				[huffFileName]: code,
-			},
-			sources: [huffFileName],
-		})
-		console.log(compiledHuff)
+		try{
+			const compiledHuff = compile({
+				files: {
+					[huffFileName]: code,
+				},
+				sources: [huffFileName],
+			})
+			setBytecode(compiledHuff.contracts.get(huffFileName).bytecode)
+		}
+		catch (e){
+			console.log(e)
+			setError(e.errors)
+		}
 	}
 
 	return (
@@ -66,10 +58,12 @@ export default function HuffVerifier() {
 				</Form.Group>
 				<Form.Group className="mb-3">
 					<Form.Label>Contract code</Form.Label>
-					<Form.Control as={'textarea'} value={code} onChange={(e) => setCode(e.target.value)} />
+					<Form.Control as={'textarea'} value={code} onChange={(e) => {setError(''); setBytecode(''); setCode(e.target.value)}} />
 				</Form.Group>
 				<Form.Group>
-					<Form.Text>Compiled contract: {bytecode}</Form.Text>
+					{
+						error? <Form.Text>{error}</Form.Text>: <Form.Text>Compiled contract: {bytecode}</Form.Text>
+					}
 				</Form.Group>
 				<Button variant="primary" type='submit'>Verify</Button>
 			</Form>
