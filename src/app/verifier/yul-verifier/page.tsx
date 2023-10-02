4f985f1adcea02c8dc94bbbdf9ee892de66bac1f
rayniel95 <rainyel.ramos@gmail.com>
2023-10-02 08:13:25 -0400

    add base code for solidity compilation

--- src/app/verifier/yul-verifier/page.tsx
+++ src/app/verifier/yul-verifier/page.tsx
@@ -3,7 +3,7 @@
 import { ethereumManager } from '@/src/stateManager/blockchainManager/ethereum'
 import { useAppSelector } from '@/src/stateManager/hooks'
 import Script from 'next/script'
-import { useRef, useState } from 'react'
+import { useCallback, useRef, useState } from 'react'
 import { Button, Col, Container, Form, Row } from 'react-bootstrap'
 import CompileInfo from '../components/CompileInfo'
 import VerifierForm from '../components/VerifierForm'
@@ -20,6 +20,68 @@ export default function YulVerifier() {
 	const [error, setError] = useState('')
 	const network = useAppSelector((state) => state.network.newtork);
 	const solcWorkerRef = useRef<null | Worker>(null)
+
+	const handleWorkerMessage = useCallback(
+		(event: MessageEvent) => {
+			const { warning, error, contracts } = event.data
+			resetExecution()
+			setContract(undefined)
+
+			if (error) {
+				log(error, 'error')
+				setIsCompiling(false)
+				return
+			}
+
+			if (warning) {
+				log(warning, 'warn')
+			}
+
+			log('Compilation successful')
+
+			if (contracts.length > 1) {
+				setIsCompiling(false)
+				log(
+					'The source should contain only one contract, Please select one to deploy.',
+					'error',
+				)
+				return
+			}
+
+			if (codeType === CodeType.Solidity) {
+				setContract(contracts[0])
+			}
+
+			if (!isExpanded) {
+				deployByteCode(contracts[0].code, '', undefined)
+			} else {
+				setIsCompiling(false)
+			}
+		},
+		[resetExecution, log, codeType, isExpanded, deployByteCode],
+	)
+
+	useEffect(() => {
+		if (solcWorkerRef && solcWorkerRef.current) {
+			// @ts-ignore change the worker message, when value and args changed.
+			solcWorkerRef.current?.onmessage = handleWorkerMessage
+		}
+	}, [solcWorkerRef, handleWorkerMessage])
+
+	useEffect(() => {
+		solcWorkerRef.current = new Worker(
+			new URL('../../lib/solcWorker.js', import.meta.url),
+		)
+		solcWorkerRef.current.onmessage = handleWorkerMessage
+		log('Solidity compiler loaded')
+
+		return () => {
+			if (solcWorkerRef?.current) {
+				solcWorkerRef.current.terminate()
+			}
+		}
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [])
 
 	function verify(e: React.FormEvent<HTMLFormElement>) {
 		e.preventDefault()
