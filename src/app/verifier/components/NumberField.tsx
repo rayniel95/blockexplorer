49f543851d1b5be3ee68c1a89eb762b5d1f8cf71
rayniel95 <rainyel.ramos@gmail.com>
2023-10-08 15:44:30 -0400

    delete not necessary attr

--- src/app/verifier/components/NumberField.tsx
+++ src/app/verifier/components/NumberField.tsx
@@ -1,6 +1,6 @@
 import { useTsController } from "@ts-react/form";
 
-export default function NumberField({ req }: { req: number }) {
+export default function NumberField() {
     const {
       field: { onChange, value },
       error,
@@ -8,7 +8,6 @@ export default function NumberField({ req }: { req: number }) {
     return (
       <>
         <span>
-          <span>{`req is ${req}`}</span>
           <input
             value={value !== undefined ? value + "" : "-1"}
             onChange={(e) => {
