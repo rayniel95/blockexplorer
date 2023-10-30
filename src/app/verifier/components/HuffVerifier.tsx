cb2b35dd772496c1d2c76eed7410d7386bf3228c
rayniel95 <rainyel.ramos@gmail.com>
2023-10-30 08:18:55 -0400

    delete not necessary code

--- src/app/verifier/components/HuffVerifier.tsx
+++ src/app/verifier/components/HuffVerifier.tsx
@@ -75,29 +75,6 @@ export default function HuffVerifier() {
         // 	<h5>Huff Verifier</h5>
         // 	<Form onSubmit={verify}>
         // 		<Form.Group className="mb-3">
-        // 			<Row>
-        // 				<Col>
-        // 					<Form.Label>Address</Form.Label>
-        // 					<Form.Control type="text" value={address} onChange={(e) => setAddress(e.target.value)} />
-        // 				</Col>
-        // 				<Col xs={2}>
-        // 					<Form.Label>At block</Form.Label>
-        // 					<Form.Control type="text" value={addressBlock} onChange={(e) => setAddressBlock(e.target.value)} />
-        // 				</Col>
-        // 			</Row>
-        // 		</Form.Group>
-        // 		<Form.Group className="mb-3">
-        // 			<Form.Label>Contract code</Form.Label>
-        // 			<Form.Control as={'textarea'} value={code} onChange={(e) => { setError(''); setBytecode(''); setMatch(""); setCode(e.target.value) }} />
-        // 		</Form.Group>
-        // 		<Form.Group>
-        // 			{
-        // 				error ? <Form.Text>{error}</Form.Text> : <Form.Text>{match}, compiled contract: {bytecode}</Form.Text>
-        // 			}
-        // 		</Form.Group>
-        // 		<Button variant="primary" type='submit'>Verify</Button>
-        // 	</Form>
-        // </Container>
     )
 }
 
